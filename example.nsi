d18c0021ffa3f3eb5118d87b93d9d433986642d9
olegphpgithub <olegphp@yandex.ru>
2019-06-11 14:29:05 +0300

    Add fill_compilers_dict function;

--- example.nsi
+++ example.nsi
@@ -1,29 +1,88 @@
-Name "setup"
+;NSIS Modern User Interface
+;Basic Example Script
+;Written by Joost Verburg
 
-OutFile "example.exe"
+;--------------------------------
+;Include Modern UI
 
-InstallDir $PROGRAMFILES\example
+  !include "MUI2.nsh"
 
+;--------------------------------
+;General
 
-CRCCheck off
+  ;Name and file
+  Name "Modern UI Test"
+  OutFile "example.exe"
 
-RequestExecutionLevel user
+  ;Default installation folder
+  InstallDir "$LOCALAPPDATA\Modern UI Test"
+  
+  ;Get installation folder from registry if available
+  InstallDirRegKey HKCU "Software\Modern UI Test" ""
 
+  ;Request application privileges for Windows Vista
+  RequestExecutionLevel user
 
-SetCompressor /SOLID lzma
+;--------------------------------
+;Interface Settings
 
-Page components
-Page instfiles
+  !define MUI_ABORTWARNING
 
+;--------------------------------
+;Pages
 
-UninstPage instfiles
+  !insertmacro MUI_PAGE_LICENSE "${NSISDIR}\Docs\Modern UI\License.txt"
+  !insertmacro MUI_PAGE_COMPONENTS
+  !insertmacro MUI_PAGE_DIRECTORY
+  !insertmacro MUI_PAGE_INSTFILES
+  
+  !insertmacro MUI_UNPAGE_CONFIRM
+  !insertmacro MUI_UNPAGE_INSTFILES
+  
+;--------------------------------
+;Languages
+ 
+  !insertmacro MUI_LANGUAGE "English"
 
-Section "example"
+;--------------------------------
+;Installer Sections
+
+Section "Dummy Section" SecDummy
+
+  SetOutPath "$INSTDIR"
+  
+  ;ADD YOUR OWN FILES HERE...
+  
+  ;Store installation folder
+  WriteRegStr HKCU "Software\Modern UI Test" "" $INSTDIR
+  
+  ;Create uninstaller
+  WriteUninstaller "$INSTDIR\Uninstall.exe"
+
 SectionEnd
 
-Function .onInit
-    
-    
-FunctionEnd
+;--------------------------------
+;Descriptions
 
+  ;Language strings
+  LangString DESC_SecDummy ${LANG_ENGLISH} "A test section."
 
+  ;Assign language strings to sections
+  !insertmacro MUI_FUNCTION_DESCRIPTION_BEGIN
+    !insertmacro MUI_DESCRIPTION_TEXT ${SecDummy} $(DESC_SecDummy)
+  !insertmacro MUI_FUNCTION_DESCRIPTION_END
+
+;--------------------------------
+;Uninstaller Section
+
+Section "Uninstall"
+
+  ;ADD YOUR OWN FILES HERE...
+
+  Delete "$INSTDIR\Uninstall.exe"
+
+  RMDir "$INSTDIR"
+
+  DeleteRegKey /ifempty HKCU "Software\Modern UI Test"
+
+SectionEnd
--- main.py
+++ main.py
@@ -6,27 +6,29 @@ from collections import OrderedDict
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SOURCE_FILENAME = os.path.join(BASE_DIR, r"example.nsi")
+COMPILED_FILENAME = os.path.join(BASE_DIR, r"example.exe")
 COMPILERS_FILENAME = os.path.join(BASE_DIR, r"list.txt")
 OUTPUT_DIRECTORY = os.path.join(BASE_DIR, r"output")
+
+
+available_compilers = OrderedDict()
 
 
 available_compressors = OrderedDict()
 available_compressors[r'zlib'] = 'zlib'
 available_compressors[r'bzip2'] = 'bzip2'
 available_compressors[r'lzma'] = 'lzma'
-available_compressors[r'/SOLID zlib'] = 'SOLID_zlib'
-available_compressors[r'/SOLID bzip2'] = 'SOLID_bzip2'
-available_compressors[r'/SOLID lzma'] = 'SOLID_lzma'
+available_compressors[r'SOLID_zlib'] = '/SOLID zlib'
+available_compressors[r'SOLID_bzip2'] = '/SOLID bzip2'
+available_compressors[r'SOLID_lzma'] = '/SOLID lzma'
 
 
-class CompilerList:
-    def __init__(self, file_path):
-        self.compiler_list = list()
-        with open(file_path, "r") as input_file:
-            input_lines = input_file.readlines()
-            input_file.seek(0)
-            for index, line in enumerate(input_lines):
-                self.compiler_list.append(line.strip())
+def fill_compilers_dict(file_path):
+    with open(file_path, "r") as input_file:
+        input_lines = input_file.readlines()
+        for index, line in enumerate(input_lines):
+            compiler_index = line.strip().split(os.sep)[2]
+            available_compilers[compiler_index] = line.strip().strip(r'"')
 
 
 def compile_file(compiler_path, source_file):
@@ -64,8 +66,19 @@ def change_compressor(source_file_path, compressor):
 
 # compile_file(r'c:\Program Files (x86)\nsis-2.46.47.1\makensis.exe', r'c:\progs\nsislearn01\__argv.nsi')
 
-for current_compressor in available_compressors.keys():
-    change_compressor(SOURCE_FILENAME, current_compressor)
+fill_compilers_dict(COMPILERS_FILENAME)
 
+for current_compiler_key in available_compilers.keys():
+    for current_compressor_key in available_compressors.keys():
+        change_compressor(SOURCE_FILENAME, available_compressors[current_compressor_key])
+        compile_file(available_compilers[current_compiler_key], SOURCE_FILENAME)
+        target_filename = os.path.basename(COMPILED_FILENAME)
+        target_filename = target_filename[:-4]
+        target_filename = "%s%s%s.%s.%s.exe" % (OUTPUT_DIRECTORY,
+                                                os.sep,
+                                                target_filename,
+                                                current_compiler_key,
+                                                current_compressor_key)
+        print(target_filename)
+        os.rename(COMPILED_FILENAME, target_filename)
 
-print("Hello, world!")
