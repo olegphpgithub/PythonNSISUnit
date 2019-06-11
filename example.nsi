886e7260edeb7222185953742f562d88082f9a53
olegphpgithub <olegphp@yandex.ru>
2019-06-11 11:57:39 +0300

    Add change_compressor function;

---
+++ example.nsi
@@ -0,0 +1,29 @@
+Name "setup"
+
+OutFile "example.exe"
+
+InstallDir $PROGRAMFILES\example
+
+
+CRCCheck off
+
+RequestExecutionLevel user
+
+
+SetCompressor /SOLID lzma
+
+Page components
+Page instfiles
+
+
+UninstPage instfiles
+
+Section "example"
+SectionEnd
+
+Function .onInit
+    
+    
+FunctionEnd
+
+
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import re
 import os
 import subprocess
+from collections import OrderedDict
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-NSIS_INPUT_FILE = os.path.join(BASE_DIR, r"list.txt")
-NSIS_OUTPUT_DIR = os.path.join(BASE_DIR, r"output")
+SOURCE_FILENAME = os.path.join(BASE_DIR, r"example.nsi")
+COMPILERS_FILENAME = os.path.join(BASE_DIR, r"list.txt")
+OUTPUT_DIRECTORY = os.path.join(BASE_DIR, r"output")
+
+
+available_compressors = OrderedDict()
+available_compressors[r'zlib'] = 'zlib'
+available_compressors[r'bzip2'] = 'bzip2'
+available_compressors[r'lzma'] = 'lzma'
+available_compressors[r'/SOLID zlib'] = 'SOLID_zlib'
+available_compressors[r'/SOLID bzip2'] = 'SOLID_bzip2'
+available_compressors[r'/SOLID lzma'] = 'SOLID_lzma'
 
 
 class CompilerList:
@@ -16,9 +29,9 @@ class CompilerList:
                 self.compiler_list.append(line.strip())
 
 
-def compile_file(compiler_path, source_path):
+def compile_file(compiler_path, source_file):
 
-    compiler_command = r'"%s" "%s"' % (compiler_path, source_path)
+    compiler_command = r'"%s" "%s"' % (compiler_path, source_file)
 
     try:
         device_null = open(os.devnull, 'wb')
@@ -38,5 +51,21 @@ def compile_file(compiler_path, source_path):
         exit(1)
 
 
-compile_file(r'c:\Program Files (x86)\nsis-2.46.47.1\makensis.exe', r'c:\progs\nsislearn01\__argv.nsi')
+def change_compressor(source_file_path, compressor):
+
+    compressor_statement = r'SetCompressor %s' % compressor
+
+    with open(source_file_path, 'r+') as source_file:
+        content_old = source_file.read()
+        content_new = re.sub(r'SetCompressor.*', compressor_statement, content_old, flags=re.M)
+        source_file.seek(0)
+        source_file.write(content_new)
+
+
+# compile_file(r'c:\Program Files (x86)\nsis-2.46.47.1\makensis.exe', r'c:\progs\nsislearn01\__argv.nsi')
+
+for current_compressor in available_compressors.keys():
+    change_compressor(SOURCE_FILENAME, current_compressor)
+
+
 print("Hello, world!")
